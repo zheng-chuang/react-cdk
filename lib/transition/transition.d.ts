f285fce35934ecf30a22f5d411017fe6f6629370
zhengchuangchuang <zhengchuangchuang@kuaishou.com>
2021-04-09 23:39:42 +0800

    init

---
+++ lib/transition/transition.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+interface TransitionProps {
+    name: string;
+    show?: boolean;
+}
+export declare const Transition: React.NamedExoticComponent<React.PropsWithChildren<TransitionProps>>;
+export {};
---
+++ src/transition/index.ts
@@ -0,0 +1,4 @@
+import { Transition } from "./transition";
+
+export default Transition;
+export { Transition };
