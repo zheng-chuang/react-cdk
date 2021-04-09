c5f2afd740a49c2902c48b27485c3dae047fa104
zhengchuangchuang <zhengchuangchuang@kuaishou.com>
2021-04-10 01:11:29 +0800

    Teleport default to body

--- packages/teleport/index.tsx
+++ packages/teleport/index.tsx
@@ -1,29 +1,29 @@
 import React, { memo, PropsWithChildren, useCallback, useReducer } from "react";
 import ReactDOM from "react-dom";
 interface TeleportProps {
-  to: string;
+  to?: string;
 }
 
 type TeleportType = React.FC<PropsWithChildren<TeleportProps>>;
 
 export const Teleport: TeleportType = memo((props) => {
   const [, forceUpdate] = useReducer((x) => x + 1, 0);
-
+  const { to="body" } = props
   const renderPortal = useCallback(() => {
     const waitForDom = () => {
-      const dom = document.querySelector(props.to);
-      if (!dom) {
+      const host = document.querySelector(to);
+      if (!host) {
         requestAnimationFrame(waitForDom);
       } else {
         forceUpdate();
       }
     };
-    const dom = document.querySelector(props.to);
-    if (!dom) {
+    const host = document.querySelector(to);
+    if (!host) {
       waitForDom();
       return null;
     }
-    return ReactDOM.createPortal(props.children, dom);
+    return ReactDOM.createPortal(props.children, host);
   }, [props.children]);
   return renderPortal();
 });
